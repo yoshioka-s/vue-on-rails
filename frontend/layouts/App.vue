79f83f26d33133be2bc86acc40607abcf2f39526
Shu Yoshioka <yoshioka63@gmail.com>
2018-10-08 21:27:41 +0900

    Install webpack with vue

---
+++ frontend/layouts/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Vue on Rails!! Imejin</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="sass">
+h1 {
+  color: blue;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './frontend/entries/index.js',
+    app: './frontend/entries/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public', 'dist'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      use: 'vue-loader'
+    }, {
+      test: /\.sass$/,
+      use: ['vue-style-loader', 'css-loader', 'sass-loader']
+    }, {
+      test: /\.css$/,
+      use: ['vue-style-loader', 'css-loader']
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin()
+  ],
+
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true
+  }
+}
